6ad4ce3846e6d37538654c3208276a26be0e589d
Koalahack <cristian.paz@correounivalle.edu.co>
2022-07-25 19:25:43 -0500

    Ejemplo

--- TRABAJO FINAL.R
+++ TRABAJO FINAL.R
@@ -1,27 +1,33 @@
 
 # 0. Paquetes ####
 
+# Para análisis
 library(forecast)
 library(MASS)
 library(urca)
 library(tseries)
 
+# Para gráficas
+library(tidyverse)
+require(cowplot)
+source("ggseries.R")
+
 # 1. Datos ####
 datos = readxl::read_xlsx("datos.xlsx")
+datos = datos[1:(nrow(datos)-2),]
+control = datos[(nrow(datos)-2):nrow(datos),]
 
+n = nrow(datos)
 
 # 2. Serie ####
 serie=ts(datos$Inflacion)
 años = datos$año
 windows()
-plot(y=serie,x=años, xlab="Añoo- mes", ylab="Millones de dolares (USD)", col="red")
 
+source("ggseries.R")
 
-windows()
-par(mfrow=c(1,3))
-plot(serie, xlab="Anho- mes", ylab="Ln Zt", col="red")
-acf(serie, lag=25,ylim=c(-1,1), main="ACF")
-acf(serie, lag=25,ylim=c(-1,1), type="partial", main="PACF")
+x11()
+graf_ts(serie,2,años,"")
 
 modelo1 <- auto.arima(serie)
 summary(modelo1)
@@ -36,12 +42,24 @@ lam1<-bc1$x[which.max(bc1$y)]  #para saber que tramsformacion hacerle segun box-
 lam1
 serie_t=log(serie)##Serie Transformada
 windows()
-plot(serie_t, xlab="Anho- mes", ylab="Ln Zt", col="red")
+grafico2 = ggplot(mapping = aes(x=años,
+                                y=serie_t)) +
+  geom_line() +
+  geom_point() +
+  xlab("años")+
+  ylab("logarito de inflaciín interanual (%)")+
+  scale_y_continuous(breaks = seq(-0.5,2.6,0.5),
+                     limits = c(-0.5,2.4))+
+  scale_x_continuous(breaks = seq(min(años),max(años)+2,3), 
+                     limits = c(min(años),max(años)))+
+  theme_bw()+
+  labs(title = "")
+grafico2
 
 windows()
 par(mfrow=c(1,3))
 plot(serie_t, xlab="Anho- mes", ylab="Ln Zt", col="red")
-acf(serie_t, lag=25,ylim=c(-1,1), main="ACF")
+acf(serie_t, lag=,ylim=c(-1,1), main="ACF")
 acf(serie_t, lag=25,ylim=c(-1,1), type="partial", main="PACF")
 
 modelo1 <- auto.arima(serie_t)
@@ -69,16 +87,13 @@ acf(dts, lag=25,ylim=c(-1,1), main="ACF")
 acf(dts, lag=25,ylim=c(-1,1), type="partial", main="PACF")
 
 
-modelo1 <- auto.arima(serie_t)
-summary(modelo1)
+modeloSt <- auto.arima(serie_t)
+summary(modeloSt)
 
-modelo1 <- auto.arima(dts)
-summary(modelo1)
+modeloDTS <- auto.arima(dts)
+summary(modeloDTS)
 
 summary(arima(dts, order = c(1,0,1), method = "CSS-ML"))
 
 x11()
 tsdiag(modelo1)
-
-
-
